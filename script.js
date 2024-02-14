8a7501d303185425b94979136e3b44df4d8eb24d
Rublyn11 <ikedichiwidom7@gmail.com>
2024-02-14 04:40:24 -0800

    Readjustment, and few changes. to the project section

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // Multiple word in Home Section Typed Js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer!', 'Blockchain Developer', 'Blogger', 'Youtuber'],
+    strings: ['Frontend Developer!', 'Blockchain Enthusiast', 'FX Trader', 'Technical Writer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
